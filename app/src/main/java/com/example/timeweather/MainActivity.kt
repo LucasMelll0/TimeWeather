4f0e47bf61896efbb42de35192d38fed0b76c890
Lucas Mello <77343991+LucasMelll0@users.noreply.github.com>
2022-05-14 15:45:52 -0300

    Refatoração da MainActivity.kt e adição de um progressBar no activity_main.xml(Não funciona do jeito esperado ainda)

--- app/src/main/java/com/example/timeweather/MainActivity.kt
+++ app/src/main/java/com/example/timeweather/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.timeweather
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.timeweather.DAO.CityCoordDAO
 import com.example.timeweather.DAO.CityDAO
+import com.example.timeweather.model.City
 import com.example.timeweather.model.CityCoord
 import com.example.timeweather.requisition.Weather
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cityList : ArrayList<City>
 
 
 
@@ -20,26 +27,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val city1 = CityCoord("-21.0422", "-41.9733")
         val city2 = CityCoord("-21.205", "-41.8878")
-
         val cityCoordDAO = CityCoordDAO()
         cityCoordDAO.save(city1)
         cityCoordDAO.save(city2)
-        val executor: ExecutorService = Executors.newSingleThreadExecutor()
-        val tempoAtual = Weather(executor)
-        tempoAtual.setCurrentWeather()
-        while (CityDAO.cities.size == 0) {
-            continue
-        }
-
-        val cityList = CityDAO.cities
 
 
 
 
-        val recyclerViewCity : RecyclerView = findViewById(R.id.recyclerview_cities)
 
-        recyclerViewCity.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewCity.adapter = CityWeatherAdapter(cityList)
+
 
 
     }
@@ -47,10 +43,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        runRequisition()
+        settingUpRecyclerView()
+        hideProgressBar()
 
     }
 
+    private fun hideProgressBar() {
+        val progressBar = findViewById<ProgressBar>(R.id.progressbar_main)
 
+        progressBar.visibility = View.GONE
+
+    }
+
+    private fun settingUpRecyclerView() {
+        val recyclerViewCity : RecyclerView = findViewById(R.id.recyclerview_cities)
+
+        recyclerViewCity.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewCity.adapter = CityWeatherAdapter(cityList)
+
+    }
+
+    private fun runRequisition() {
+        val executor: ExecutorService = Executors.newSingleThreadExecutor()
+        val tempoAtual = Weather(executor)
+        tempoAtual.setCurrentWeather()
+        while (CityDAO.cities.size == 0) {
+            continue
+        }
+
+       cityList = CityDAO.cities
+
+    }
 
 
 }
